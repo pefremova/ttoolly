e9965cdd1b5ac49525472e8e9f2e2fad56cfb99b
Polina Efremova <pefremova@gmail.com>
2016-10-24 12:31:09 +0300

    add Gb to convert_size_to_bytes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='ttoolly',
-      version='0.11.6',
+      version='0.11.7',
       description="Django test tools",
       include_package_data=True,
       packages=find_packages(),
