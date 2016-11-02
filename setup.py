d6114119c21bc007c3960361704ad43cf70475b2
Polina Efremova <pefremova@gmail.com>
2016-11-02 17:31:29 +0300

    version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='ttoolly',
-      version='0.11.7',
+      version='0.11.8',
       description="Django test tools",
       include_package_data=True,
       packages=find_packages(),
