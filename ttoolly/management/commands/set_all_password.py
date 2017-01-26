16360e5c2db661679d3ff0c70ebec126d1eca54f
Polina Efremova <pefremova@gmail.com>
2017-01-26 13:11:19 +0300

    password as option in set_all_password command

--- ttoolly/management/commands/set_all_password.py
+++ ttoolly/management/commands/set_all_password.py
@@ -15,10 +15,10 @@ class Command(BaseCommand):
     help = "Changes all users passwords to specified (qwerty by default)"
 
     def add_arguments(self, parser):
-        parser.add_argument('password', nargs='?', default='qwerty', help='New password. Default: %(default)s')
+        parser.add_argument('-p', '--password', default='qwerty',  help='New password. Default: %(default)s')
 
     def handle(self, *args, **kwargs):
-        password = kwargs.get('password') or 'qwerty'
+        password = kwargs.get('password')
 
         for user in get_user_model().objects.all():
             user.set_password(password)
