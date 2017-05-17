289ea0db16494ab3dd12fab75ca4c43b8a2f93d5
Polina Efremova <pefremova@gmail.com>
2017-05-17 18:14:39 +0300

    use pks in udumpdata

--- ttoolly/management/commands/udumpdata.py
+++ ttoolly/management/commands/udumpdata.py
@@ -10,19 +10,24 @@ get_model = apps.get_model
 class Command(BaseCommand):
 
     help = ("Write to file the contents of the database as a fixture with "
-            "readable unicode text")
+            "readable unicode text\n"
+            "Example: manage.py udumpdata accounts.User -f /path/to/file")
     args = 'appname.ModelName'
 
     def add_arguments(self, parser):
         parser.add_argument('label')
         parser.add_argument('-f', '--file', dest='path_to_file', help='Specifies the output file path.', required=True)
+        parser.add_argument('--pk', dest='pks', nargs='+', help='Specifies pks of objects')
 
     def handle(self, *label, **kwargs):
         path_to_file = kwargs.get('path_to_file')
         app_label, model_label = kwargs.get('label').split('.')
         obj_model = get_model(app_label, model_label)
+        qs = obj_model.objects.all()
+        if kwargs.get('pks'):
+            qs = qs.filter(pk__in=kwargs.get('pks'))
         text = unicode_to_readable(serializers.serialize('json',
-                                                         obj_model.objects.all(), indent=4,
+                                                         qs, indent=4,
                                                          use_natural_foreign_keys=True))
         with open(path_to_file, 'ab') as f:
             f.write(text.encode('utf-8'))
