9b775b302c5919c4edcd485a26f8d5852ad9b15c
Polina Efremova <pefremova@gmail.com>
2017-10-15 20:40:45 +0300

    use udumpdata with django-polymorphic

--- ttoolly/management/commands/udumpdata.py
+++ ttoolly/management/commands/udumpdata.py
@@ -23,11 +23,21 @@ class Command(BaseCommand):
         path_to_file = kwargs.get('path_to_file')
         app_label, model_label = kwargs.get('label').split('.')
         obj_model = get_model(app_label, model_label)
+
         qs = obj_model.objects.all()
         if kwargs.get('pks'):
             qs = qs.filter(pk__in=kwargs.get('pks'))
+        objects_list = list(qs)
+
+        if 'PolymorphicModelBase' in [b.__name__ for b in obj_model.__class__.__bases__]:
+            polymorphic_model = obj_model.polymorphic_ctype.field.model
+            polymorphic_model = polymorphic_model if polymorphic_model != obj_model else obj_model
+            base_objects_qs = polymorphic_model.base_objects.filter(
+                pk__in=qs.values_list('pk', flat=True))
+            objects_list.extend(base_objects_qs)
         text = unicode_to_readable(serializers.serialize('json',
-                                                         qs, indent=4,
+                                                         objects_list, indent=4,
                                                          use_natural_foreign_keys=True))
+
         with open(path_to_file, 'ab') as f:
             f.write(text.encode('utf-8'))
