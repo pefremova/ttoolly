6dbf60dd7f15ed6560eb2e4a4e97737f3a7b613a
Polina <pefremova@gmail.com>
2017-11-16 17:32:12 +0300

    больше информации о тесте (вместе с class.__doc__)

--- ttoolly/__init__.py
+++ ttoolly/__init__.py
@@ -12,10 +12,15 @@ if getattr(settings, 'COLORIZE_TESTS', False):
 
     def _getDescription(self, test):
         doc_first_line = test.shortDescription()
-        full_text = "\x1B[38;5;230m" + str(test) + "\x1B[0m"
+        full_text = "\033[38;5;230m" + str(test) + "\033[38;0m"
+        class_doc = test.__class__.__doc__
+        if class_doc:
+            full_text += '\n~~~~~~~~~~\n' + '\n'.join([line.strip() for line in
+                                                       to_bytes(class_doc).decode('utf-8').strip('\n ').splitlines()]) + '\n~~~~~~~~~~'
         doc = getattr(test, '_testMethodDoc', '')
         if doc:
-            full_text += '\n' + to_bytes(doc).decode('utf-8').replace('@note: ', '').strip('\n')
+            full_text += '\n  ' + '\n  '.join([line.strip() for line in
+                                               to_bytes(doc).decode('utf-8').replace('@note: ', '').strip('\n').splitlines()])
         elif self.descriptions and doc_first_line:
             full_text += '\n' + to_bytes(doc_first_line).decode('utf-8')
         return full_text
