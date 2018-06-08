2d6b1f9d0a2d84b2eb7d0325ff14b0cae7b22e56
Polina <pefremova@gmail.com>
2018-06-08 17:56:41 +0300

    upd setup

--- setup.py
+++ setup.py
@@ -3,8 +3,22 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='ttoolly',
-      version='0.23.2',
+      version='0.23.3',
       description="Django test tools",
+      author="Polina Efremova",
+      author_email="pefremova@gmail.com",
+      keywords=["django", "testing", "test tool"],
       include_package_data=True,
       packages=find_packages(exclude=["tests", "test_project"]),
-      install_requires=['django>=1.8', 'psycopg2', 'lxml', 'chardet', 'Pillow', 'rstr', 'future', 'freezegun'])
+      install_requires=['django>=1.8', ' psycopg2-binary', 'lxml', 'chardet', 'Pillow', 'rstr', 'future', 'freezegun'],
+      classifiers=(
+          "Framework :: Django",
+          "Framework :: Django :: 1.10",
+          "Framework :: Django :: 1.11",
+          "Framework :: Django :: 1.8",
+          "Framework :: Django :: 1.9",
+          "Framework :: Django :: 2.0",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6"
+      ),)
