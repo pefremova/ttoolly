6817cd4d481e0ca2008395fb069f5bed33830e1c
Polina <pefremova@gmail.com>
2019-03-13 12:37:11 +0300

    udumpdata for multiple models

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version[0] == '2':
     install_requires.append('functools32')
 
 setup(name='ttoolly',
-      version='0.30.0',
+      version='0.30.1',
       description="Django test tools",
       long_description=readme(),
       author="Polina Efremova",
