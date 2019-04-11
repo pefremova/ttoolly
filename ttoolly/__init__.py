662187a88d6382face255763fc741c1360f252ec
Polina <pefremova@gmail.com>
2019-04-11 19:12:00 +0300

    show full description always not only if COLORIZE_TESTS is True

--- ttoolly/__init__.py
+++ ttoolly/__init__.py
@@ -8,24 +8,25 @@ from future.utils import PY2
 from ttoolly.utils import unicode_to_readable, to_bytes
 
 
-if getattr(settings, 'COLORIZE_TESTS', False):
+def _getDescription(self, test):
+    doc_first_line = test.shortDescription()
+    full_text = str(test)
+    if getattr(settings, 'COLORIZE_TESTS', False):
+        full_text = "\033[38;5;230m" + full_text + "\033[38;0m"
+    class_doc = test.__class__.__doc__
+    if class_doc:
+        full_text += '\n~~~~~~~~~~\n' + '\n'.join([line.strip() for line in
+                                                   to_bytes(class_doc).decode('utf-8').strip('\n ').splitlines()]) + '\n~~~~~~~~~~'
+    doc = getattr(test, '_testMethodDoc', '')
+    if doc:
+        full_text += '\n  ' + '\n  '.join([line.strip() for line in
+                                           to_bytes(doc).decode('utf-8').replace('@note: ', '').strip('\n').splitlines()])
+    elif self.descriptions and doc_first_line:
+        full_text += '\n' + to_bytes(doc_first_line).decode('utf-8')
+    return full_text
 
-    def _getDescription(self, test):
-        doc_first_line = test.shortDescription()
-        full_text = "\033[38;5;230m" + str(test) + "\033[38;0m"
-        class_doc = test.__class__.__doc__
-        if class_doc:
-            full_text += '\n~~~~~~~~~~\n' + '\n'.join([line.strip() for line in
-                                                       to_bytes(class_doc).decode('utf-8').strip('\n ').splitlines()]) + '\n~~~~~~~~~~'
-        doc = getattr(test, '_testMethodDoc', '')
-        if doc:
-            full_text += '\n  ' + '\n  '.join([line.strip() for line in
-                                               to_bytes(doc).decode('utf-8').replace('@note: ', '').strip('\n').splitlines()])
-        elif self.descriptions and doc_first_line:
-            full_text += '\n' + to_bytes(doc_first_line).decode('utf-8')
-        return full_text
 
-    TextTestResult.getDescription = _getDescription
+TextTestResult.getDescription = _getDescription
 
 if PY2:
     def _printErrorList(self, flavour, errors):
