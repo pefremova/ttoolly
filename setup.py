0f77a1dfe7802284aab5d3f4bd6fafc065fdb380
Polina Efremova <pefremova@gmail.com>
2020-12-28 14:32:18 +0300

    version up

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version[0] == '2':
     install_requires.append('functools32')
 
 setup(name='ttoolly',
-      version='0.36.0',
+      version='0.36.1',
       description="Django test tools",
       long_description=readme(),
       author="Polina Efremova",
