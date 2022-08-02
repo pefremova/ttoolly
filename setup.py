e5786ba650ea8b0cdf80b131301394fe922c30e8
Polina Mishchenko <pefremova@gmail.com>
2022-08-02 16:35:13 +0300

    0.37.14

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.version[0] == '2':
 
 setup(
     name='ttoolly',
-    version='0.37.13',
+    version='0.37.14',
     description="Django test tools",
     long_description=readme(),
     author="Polina Efremova",
