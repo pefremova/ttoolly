7444b0d5f768025a932161d5f607e6d24d63ba8c
a.erunov <a.erunov@netrika.ru>
2024-06-10 11:11:47 +0300

    added support for django>=4.0

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+try:
+    from django.urls import re_path as url
+except ImportError:
+    # Django < 4.0
+    from django.conf.urls import url
 
 from test_project.test_app.views import SomeModelCreateView, SomeModelUpdateView, SomeModelDeleteView
 
